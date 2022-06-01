ed9282847a0929f7562aeabd1417961250ee7a83
Utkarsh Malik <utkarsh.malik@sendinblue.com>
2022-06-01 09:59:01 +0530

    new changes

--- lua/modules/automations.lua
+++ lua/modules/automations.lua
@@ -1,8 +1,32 @@
+local popup = require("plenary.popup")
+local v = require 'semver'
 local Automation = {}
 
 function Automation.GitPush()
 	local currBranch = string.gsub( Automation.execCommand("git branch --show-current"), '%s+', '') 
 	print(Automation.execCommand("git push origin "..currBranch))
+end
+
+function Automation.GitPull()
+	local currBranch = string.gsub( Automation.execCommand("git branch --show-current"), '%s+', '') 
+	print(Automation.execCommand("git pull origin "..currBranch))
+end
+
+function Automation.ReleaseTag()
+	local currBranch = string.gsub( Automation.execCommand("git branch --show-current"), '%s+', '') 
+	--stash current work
+	print("Stash: " .. Automation.execCommand("git stash && git fetch --tags"))
+	-- pull latest
+	print("Pull latest: " .. Automation.execCommand("git checkout dev && git pull origin dev && git checkout main && git pull origin main") .. "\n")
+	-- merge dev
+	print("Merge: " .. Automation.execCommand("git merge dev --no-ff -X thiers --no-edit") .. "\n")
+	local lastTag = Automation.execCommand("git describe --tags --abbrev=0")
+	local newTag = v(lastTag):nextPatch()
+	print("Stash: " .. Automation.execCommand("git tag v"..tag))
+end
+
+function Automation.incrementTag(lastTag)
+	local tag = split(lastTag, ".")	
 end
 
 function Automation.execCommand(command)
