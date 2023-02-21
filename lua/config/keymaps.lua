d82492413d1d219f57928101b591dd579c2476af
Utkarsh Malik <utkarsh.malik@sendinblue.com>
2023-02-21 10:26:54 +0530

    lazyvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.api.nvim_set_keymap(
+  "n",
+  "<Leader>ft",
+  "<cmd>ToggleTerm<cr>",
+  { noremap = true, silent = true, desc = "Open Toggle term" }
+)
---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,18 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = { "mason.nvim" },
+  opts = function()
+    local nls = require("null-ls")
+    return {
+      sources = {
+        -- nls.builtins.formatting.prettierd,
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.gofumpt,
+        nls.builtins.formatting.goimports,
+        nls.builtins.diagnostics.golangci_lint,
+        nls.builtins.diagnostics.flake8,
+      },
+    }
+  end,
+}
